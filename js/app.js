26d18d8f61b4561868d773d31d5af99c39fe52d8
hectormunozg <hectormunozgarcia@gmail.com>
2015-06-23 08:15:57 +0100

    final changes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 // Simple lightbox
+var imageUrl     = 0;
+var imagePath    = '#gallery > .container > .row > div > a > img';
+var overlay      = $('<div id="overlay"></div>'); 
+var image 		 = $('<img>');
 
+overlay.append(image);
+$('body').append(overlay);
 // On mouse click - display overlay 
+$(imagePath).click(function() {
 	// get image url
+	imageUrl = $(this).attr('src');
+	// toggle overlay
+	$('#overlay').toggle();
 	// show image in overlay
+	image.attr('src', imageUrl);
+	// closing overlay if it is visible
+});
 
-// On mouse click - hide overlay 
+// if overlay is visible hide it on click
+overlay.click(function() {
+	overlay.hide();
+});
+
 
